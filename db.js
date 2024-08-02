754cf71a4291e056306116dbd5384e2643947ad8
piyushhkashyap <timepass9105@gmail.com>
2024-08-02 15:10:17 +0530

    nextversion

--- db.js
+++ db.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose')
-const mongoURL = 'mongodb://localhost:27017/hotels'
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// const mongoURL = 'mongodb://localhost:27017/hotels'
+const mongoURL = 'mongodb://piyushh:okokok@ac-wbd2dgc-shard-00-00.mlnjj0c.mongodb.net:27017,ac-wbd2dgc-shard-00-01.mlnjj0c.mongodb.net:27017,ac-wbd2dgc-shard-00-02.mlnjj0c.mongodb.net:27017/?ssl=true&replicaSet=atlas-z8zou8-shard-0&authSource=admin&retryWrites=true&w=majority&appName=firstCluster';
+// const mongoURL=process.env.MONGODB_URL;
+
+
 mongoose.connect(mongoURL, {
     // userNewURLParser: true,
     // useUnifiedTopology: true,
-    // userFindandModify :false,
+    // userFindandModify :false
 })
 
 const db = mongoose.connection;
--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json())
+const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
     res.send('Welcome to my hotel')
 })
-
-
-
 // import router files 
 const personRoutes = require('./routes/personRoutes');
 const menuItemRoutes = require('./routes/menuItemRoutes')
 
-app.use('/person', personRoutes)
+app.use('/person', personRoutes) 
 app.use('/menu', menuItemRoutes)
 
-app.listen(3000, () => {
-    console.log('listening on port 3000');
-}) 
-
-// hscbcbsicbasiccnisdbcsucbosucbsucboaujdcdhbdhj
+app.listen(PORT, () => { 
+    console.log('listening on port 3000'); 
+})  
